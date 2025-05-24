5c79eaf6cd70bc824fef3efb0da238324e671b5f
marwa1454 <marwachan25@gmail.com>
2025-05-24 16:40:17 +0300

    Correction de DATABASE_URL et mise à jour des dépendances

--- DBB.py
+++ DBB.py
@@ -12,6 +12,10 @@ DB_USERNAME = os.getenv("DB_USERNAME")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_NAME = os.getenv("DB_NAME")
 
+# Vérifier que les variables sont définies
+if not all([DB_USERNAME, DB_PASSWORD, DB_NAME]):
+    raise ValueError("Les variables d'environnement DB_USERNAME, DB_PASSWORD ou DB_NAME ne sont pas définies.")
+
 # Définir la DATABASE_URL
 DATABASE_URL = f"mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@localhost:3307/{DB_NAME}"
 
